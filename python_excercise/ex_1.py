130b3493de8af2fba376561cef4ff7509e61a8d2
Kaustubh Chalke <chalkekaustubh@gmail.com>
2022-02-23 00:47:44 -0500

    Python Exercise

---
+++ python_excercise/ex_1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 22:21:53 2022
+
+@author: Kaustubh Chalke
+"""
+
+#Sum of list with iteration
+l=[1,5,3,8,2,1]
+tot=0
+for i in l:
+    tot=tot+i
+print("Sum =", tot)
+
+
+
+#Sum of list with sum()
+l=[1,5,3,8,2,1]
+#tot=sum(l)
+print("Sum=",sum(l))
+
+
+#Largest element in the list
+l=[1,5,3,8,2,1]
+print("Largest elemtt =", max(l))
